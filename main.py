9e86fe1b1719ccb8e7f576e6e9126171720053f4
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-10-28 20:25:12 +0200

    Implement crossover
    Implementation of one-point crossover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import List
 from sequence import Sequence, ARITHMETIC_OPERATORS
 import operators.crossover
+from operators.crossover import CrossoverOperator
 import operators.mutation
 import operators.selection
 from operators.selection import SelectionOperator
@@ -10,8 +11,10 @@ VALUES = [75, 3, 1, 4, 50, 6, 12, 8]
 POPULATION_SIZE = (len(VALUES)-1) * 2
 TARGET_VALUE = 852
 MINIMIZE = True
+CROSSOVER_P1 = 0.5
 
 selection_method: SelectionOperator = operators.selection.TournamentSelection
+crossover_operator: CrossoverOperator = operators.crossover.OnePointCrossOver
 
 def fitness_function(sequence: Sequence):
     return abs(sequence.get_value() - TARGET_VALUE)
@@ -32,6 +35,21 @@ def generate_population() -> List[Sequence]:
 
     return population
 
+def do_crossover(the_population: List[Sequence]):
+    offspring = []
+
+    while len(the_population) >= 2:
+        parent1 = the_population.pop()
+        parent2 = the_population.pop()
+        offspring.extend(crossover_operator.crossover(parent1, parent2, CROSSOVER_P1))
+    if the_population:
+        parent1 = the_population.pop()
+        assert len(the_population) == 0
+        offspring.append(parent1)
+    
+    return offspring
+    
+
 def main():
     # Generate the initial population
     population: List[Sequence] = generate_population()
@@ -43,8 +61,8 @@ def main():
         # Reproduce (select) the best solutions within the population
         selected_population = selection_method.select(population, MINIMIZE)
 
-        # TODO Crossover between chosen parents (crossover)
-
+        # Crossover between the best solutions chosen (parents)
+        do_crossover(selected_population.copy())
 
         # TODO Mutate generated children (mutation)
         
--- operators/crossover.py
+++ operators/crossover.py
@@ -1,9 +1,37 @@
 from abc import ABC, abstractmethod
 from sequence import Sequence
 from typing import List
+import random
+
 
 """Strategy interface for all crossover operators"""
 class CrossoverOperator(ABC):
     @abstractmethod
     def crossover(parent1: Sequence, parent2: Sequence, p1: float) -> List[Sequence]:
         pass
+
+
+class OnePointCrossOver(CrossoverOperator):
+    @staticmethod
+    def crossover(parent1: Sequence, parent2: Sequence, p1: float=0.5) -> List[Sequence]:
+        operators1 = parent1.get_operators()
+        operators2 = parent2.get_operators()
+
+        assert len(operators1) == len(operators2)
+
+        crossover_point = random.randint(0, len(operators1))
+
+        children = [
+            Sequence(
+                operators1[:crossover_point] + operators2[crossover_point:]
+            ),
+
+            Sequence(
+                operators2[:crossover_point] + operators1[crossover_point:]
+            )
+        ]
+
+        assert len(children) == 2
+        assert len(children[0].get_operators()) == len(children[1].get_operators())
+
+        return children
