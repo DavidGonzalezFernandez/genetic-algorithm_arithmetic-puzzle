2cacaf07980340a3082e5f18f96e0fc6f1fa1521
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-10-28 17:21:31 +0200

    Implementation of Sequence class

--- sequence.py
+++ sequence.py
@@ -1,8 +1,20 @@
-OPERATORS = ["+", "-", "*", "/"]
+ARITHMETIC_OPERATORS = ["+", "-", "*", "/"]
+from typing import List
+
+def calculate_operation_result(operators: List[str], values: List[int]):
+    assert len(operators) > 0
+    assert len(operators)+1 == len(values)
+
+    res = values[0]
+    for operator,value in zip(operators, values[1:]):
+        res = eval(f"{res}{operator}{value}")
+    
+    return res
 
 class Sequence:
-    def __init__(self, operators):
+    def __init__(self, operators: List[str], values: List[int]):
         self.operators = operators
+        self.value = calculate_operation_result(operators, values)
 
-    def get_value():
-        pass
+    def get_value(self):
+        return self.value
