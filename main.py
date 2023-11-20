d29003088a659906101798b0a5d39acc1a4334e1
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-11-20 16:31:25 +0100

    Add restrictions according to professor

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from operators.mutation import MutationOperator
 import operators.selection
 from operators.selection import SelectionOperator
 from population import PopulationGenerator
+import random
 
 VALUES = [75, 3, 1, 4, 50, 6, 12, 8]
 OPERATOR_LIST_SIZE = len(VALUES)-1
@@ -19,11 +20,18 @@ CROSSOVER_P1 = 0.5
 MUTATION_P = 0.1
 MAX_ITERATIONS = 100
 
+
+# TODO implement fixed and variable value
+m = POPULATION_SIZE // 2
+
 population_generator: PopulationGenerator = sequence.sequence_generator.RandomSequencePopulationGenerator
 individual_evaluator: IndividualEvaluator = sequence.sequence_evaluator.SequenceEvaluator(TARGET_VALUE, VALUES)
-selection_method: SelectionOperator = operators.selection.TournamentSelection
-crossover_operator: CrossoverOperator = operators.crossover.OnePointCrossOver
+selection_method: SelectionOperator = operators.selection.DeterministicSelector
+crossover_operator: CrossoverOperator = operators.crossover.OnePointRandomCrossOver
 mutation_operator: MutationOperator = operators.mutation.StringMutation
+
+# TODO: implement nicely
+crossover_threshold = 1
 
 
 """Loops over the population and calls the appropriate evaluaton"""
@@ -33,17 +41,23 @@ def evaluate_population(population: List[Individual]) -> None:
 
 
 """Loops over the the population and breeds the individuals"""
-def do_crossover(the_population: List[Individual]) -> List[Individual]:
+def do_crossover(the_population: List[Individual], crossover_threshold: float) -> List[Individual]:
+    assert len(the_population) % 2 == 0
+
+    random.shuffle(the_population)
+
     offspring: List[Individual] = []
 
-    while len(the_population) >= 2:
+    while len(the_population) >= 0:
         parent1 = the_population.pop()
         parent2 = the_population.pop()
-        offspring.extend(crossover_operator.crossover(parent1, parent2, CROSSOVER_P1))
-    if the_population:
-        parent1 = the_population.pop()
-        assert len(the_population) == 0
-        offspring.append(parent1)
+
+        # Threshold for crossover
+        if random.random() <= crossover_threshold:
+            offspring.extend(crossover_operator.crossover(parent1, parent2, CROSSOVER_P1))
+        else:
+            pass
+            # TODO: check what to do in this case
     
     return offspring
 
@@ -56,7 +70,6 @@ def mutate_population(the_population: List[Individual]) -> None:
 
 """General structure for a genetic algorithm"""
 def main():
-    # TODO generar la población
     # Generate the initial population
     population: List[Individual] = population_generator.generate(OPERATOR_LIST_SIZE, POPULATION_SIZE)
 
@@ -66,13 +79,20 @@ def main():
     n_generation = 0
 
     while n_generation<MAX_ITERATIONS  and  not any(individual.get_fitness_value()==0 for individual in population):
-        # TODO: Select the best solutions within the population
-        selected_population: List[Individual] = selection_method.select(population, MINIMIZE)
+        assert m>0  and  m % 2 == 0
+        assert len(population) % 2 == 0
 
-        # TODO: Crossover between the best solutions chosen (parents)
-        offspring: List[Individual] = do_crossover(selected_population.copy())
+        # Select the best solutions within the population
+        selected_population: List[Individual] = selection_method.select(population, m, MINIMIZE)
+        assert len(selected_population) % 2 == 0
+        assert len(selected_population) == m
 
-        # TODO Mutate generated children
+        # Crossover between the best solutions chosen (parents)
+        offspring: List[Individual] = do_crossover(selected_population.copy(), crossover_threshold)
+        assert len(offspring) % 2 == 0
+        assert len(offspring) <= len(selected_population)
+
+        # Mutate generated children
         mutate_population(offspring)
 
         # TODO Update population list (replacing the 'worst' with all the )
