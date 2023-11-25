6d47fa65eca8c119e57d83afaf47005828c88372
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-11-25 09:47:04 +0100

    Document simulation

--- simulation.py
+++ simulation.py
@@ -6,10 +6,15 @@ from operators.selection import SelectionOperator
 from population import BestSelector
 import random
 
-def check_only_instance(population: List[Individual]):
-    for individual in population:
-        assert population.count(individual) == 1
 
+"""Checks that the count for each Individual in the population is 1
+If 2 Individuals are have the same values (==) they should not be the same reference (is)"""
+def check_only_instance(population: List[Individual]) -> None:
+    assert all((population.count(individual) == 1 for individual in population))
+
+
+"""Given an original population, the new offspring (<=population). It removes the 'worst'
+individuals from the population, according to the selector and adds all the new_offspring."""
 def replace_worst_with_offpring(
     population: List[Individual],
     offspring: List[Individual],
@@ -24,12 +29,18 @@ def replace_worst_with_offpring(
 
 
 """Loops over the population and mutates the individuals. It modifies the objects"""
-def mutate_population(the_population: List[Individual], mutation_operator: MutationOperator, MUTATION_P) -> None:
+def mutate_population(
+    the_population: List[Individual],
+    mutation_operator: MutationOperator,
+    mutation_threshold: float
+)-> None:
+    assert 0 <= mutation_threshold <= 1
     for individual in the_population:
-        mutation_operator.mutate(individual, MUTATION_P)
+        mutation_operator.mutate(individual, mutation_threshold)
 
 
-"""Loops over the population and calls the appropriate evaluaton"""
+"""Loops over the population and evaluates all the individuals,
+by calling the given evaluator"""
 def evaluate_population(
     population: List[Individual], 
     individual_evaluator: IndividualEvaluator
@@ -45,9 +56,9 @@ def do_crossover(
     crossover_operator: CrossoverOperator
 ) -> List[Individual]:
     assert len(the_population) % 2 == 0
+    assert 0 <= crossover_threshold <= 1 
 
     random.shuffle(the_population)
-
     offspring: List[Individual] = []
 
     while len(the_population) > 0:
@@ -66,16 +77,17 @@ def do_crossover(
 def run_simulation(
     population: List[Individual],
     MAX_ITERATIONS: int,
-    m,
-    minimize,
-    individual_evaluator,
+    m: int,
+    minimize: bool,
+    individual_evaluator: IndividualEvaluator,
     selection_method: SelectionOperator,
-    crossover_operator,
-    crossover_threshold,
-    mutation_operator,
-    mutation_prob,
-    best_selector,
+    crossover_operator: CrossoverOperator,
+    crossover_threshold: float,
+    mutation_operator: MutationOperator,
+    mutation_threshold: float,
+    best_selector: BestSelector,
 ):
+    # Seed for reproducibility
     random.seed(0)
 
     # Evaluate the initial population
@@ -100,7 +112,7 @@ def run_simulation(
         assert len(offspring) <= len(selected_population)
 
         # Mutate generated children
-        mutate_population(offspring, mutation_operator, mutation_prob)
+        mutate_population(offspring, mutation_operator, mutation_threshold)
         check_only_instance(offspring)
 
         # Update population list (replacing the 'worst' with all the new children)
@@ -111,3 +123,4 @@ def run_simulation(
         evaluate_population(population, individual_evaluator)
 
         n_generation += 1
+        # TODO update M
